3fa17e2dd70ddc6de64c845db3b3e80d84884655
xhxcx <xxhxcxx.sn@gmail.com>
2021-01-12 17:48:51 +0100

    Fix FareCalculator to validate cases : parking for less than an hour and more than 24h

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -3,19 +3,34 @@ package com.parkit.parkingsystem.service;
 import com.parkit.parkingsystem.constants.Fare;
 import com.parkit.parkingsystem.model.Ticket;
 
+import java.time.Duration;
+
+/**
+ * Fare calculator to set price for a given Ticket
+ *
+ */
 public class FareCalculatorService {
 
+    /**
+     * Calculate a fee based on a Ticket
+     * get entry and exit hours
+     * then multiply the duration of stay by the rate regarding the parking type
+     *
+     * @param ticket
+     */
     public void calculateFare(Ticket ticket){
         if( (ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime())) ){
             throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
         }
 
-        int inHour = ticket.getInTime().getHours();
-        int outHour = ticket.getOutTime().getHours();
+        //Get entry and exit hours in millis
+        long inHour = ticket.getInTime().getTime();
+        long outHour = ticket.getOutTime().getTime();
 
-        //TODO: Some tests are failing here. Need to check if this logic is correct
-        int duration = outHour - inHour;
+        //Calculate the duration and convert it from millis to hours by dividing by 3600*1000
+        double duration = (double)Duration.ofMillis(outHour - inHour).toMillis()/3600000;
 
+        //Apply fare regarding parking type
         switch (ticket.getParkingSpot().getParkingType()){
             case CAR: {
                 ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR);
